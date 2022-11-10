c2b80ba04c0c51554acd2eb946fe7fdf01bcf00a
Richard Santos <104379480+RichardSaaa@users.noreply.github.com>
2022-11-10 17:45:15 -0300

    Update Ex2.java
    Correção de bug do one drive.

---
+++ Ex2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main (String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int i;
+
+        for(i=30; i>0; i--) {
+            System.out.println(i);
+        }
+      System.out.println("Explosão");
+
+    } 
+} 
+
+/* 
+
+while(i < 30) {
+    System.out.println(i);
+    i--;
+}
+
+
+
+Do While 
+
+i = 30;
+do {
+    System.out.println(i);
+    i--;
+} while(i > 0);
+*/
